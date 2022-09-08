43cda0fc45a2b476422e12c2cec1f762686a0083
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2022-09-08 23:24:00 +0300

    Изменен header (кнопки спрятаны в реализованое бургер меню)

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,9 @@
 import "../scss/style.scss";
 
 // Бургер-меню
-let body = document.querySelector("body")
-let menuBtn = document.querySelector(".icon-menu")
-let menu = document.querySelector(".menu__body")
-menuBtn.addEventListener("click", function () {
-	menu.classList.toggle("active")
-	menuBtn.classList.toggle("active")
-	body.classList.toggle("no-scroll")
-})
-
+document.querySelector('.more-button').addEventListener('click', function () {
+	document.querySelector('.list-container').classList.toggle('active');
+});
 // Плавный скролл до элемента
 
 const smoothLinks = document.querySelectorAll("a[href*='#']");
